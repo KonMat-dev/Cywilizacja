10bdb6b27bd2c5d0f27b03d24d13519dd477530f
Konrad <61757091+KonMat-dev@users.noreply.github.com>
2020-12-16 20:30:53 +0100

    Mapa

---
+++ Cywilizacja/Assets/Skrypt/BattaleControler.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BattaleControler : MonoBehaviour
+{
+    FieldMenager fieldMenager = new FieldMenager();
+    [SerializeField] int testInt;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        fieldMenager.RowHeight = 5;
+        testInt = fieldMenager.RowHeight;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

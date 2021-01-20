4464ecaa09062fa61d6ec21effeb9c0760eb5bcc
Nardox121 <uminski.marcin1@gmail.com>
2021-01-20 20:55:24 +0100

    connecting players through lobby

---
+++ Cywilizacja/Assets/Skrypt/GameState.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assets.Skrypt
+{
+    public class GameState
+    {
+        public int gold1;
+        public int cityLvl1;
+        public int cityCurrentHP1;
+        public int cityMaxHP1;
+        public List<Hero> heroes1;
+
+        public int gold2;
+        public int cityLvl2;
+        public int cityCurrentHP2;
+        public int cityMaxHP2;
+        public List<Hero> heroes2;
+    }
+}
---
+++ Cywilizacja/Assets/Skrypt/MultiplayerMenager.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using SWNetwork;
+using Assets.Skrypt;
+
+public class MultiplayerMenager : MonoBehaviour
+{
+    public RemoteEventAgent remoteEventAgent;
+
+    private void Start()
+    {
+        remoteEventAgent = gameObject.GetComponent<RemoteEventAgent>();
+    }
+
+    public void sendMessage(GameState gameState)
+    {
+        SWNetworkMessage message = new SWNetworkMessage();
+        message.Push(gameState.gold1);
+        message.Push(gameState.cityLvl1);
+        message.Push(gameState.cityCurrentHP1);
+        message.Push(gameState.cityMaxHP1);
+
+        message.Push(gameState.gold2);
+        message.Push(gameState.cityLvl2);
+        message.Push(gameState.cityCurrentHP2);
+        message.Push(gameState.cityMaxHP2);
+
+        this.remoteEventAgent.Invoke("ChangedGameState", message);
+    }
+
+    public void applyChanges(GameState gameState, SWNetworkMessage message)
+    {
+        gameState.gold1 = message.PopByte();
+        gameState.cityLvl1 = message.PopByte();
+        gameState.cityCurrentHP1 = message.PopByte();
+        gameState.cityMaxHP1 = message.PopByte();
+
+        gameState.gold2 = message.PopByte();
+        gameState.cityLvl2 = message.PopByte();
+        gameState.cityCurrentHP2 = message.PopByte();
+        gameState.cityMaxHP2 = message.PopByte();
+    }
+}
